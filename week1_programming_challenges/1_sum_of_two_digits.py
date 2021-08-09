c760f83e348b03d7fec0216ce809b26cbf1065d9
Sergey Pastukhov <pastukhowsi@gmail.com>
2021-08-10 01:32:48 +0300

    Let's get it started

---
+++ week1_programming_challenges/1_sum_of_two_digits.py
@@ -0,0 +1,15 @@
+def ab_sum(numbers_list):
+
+    sum_number = sum(numbers_list)
+
+    return sum_number
+
+
+if __name__ == '__main__':
+
+    input_string = input().split()
+    numbers = [int(el) for el in input_string]
+
+    s = ab_sum(numbers)
+
+    print(s)
