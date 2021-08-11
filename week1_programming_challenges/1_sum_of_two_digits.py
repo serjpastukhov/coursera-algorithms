545ed377ed66750bb49717e81980746039ef0554
Sergey Pastukhov <pastukhowsi@gmail.com>
2021-08-11 12:24:41 +0300

    trying git from laptop

--- week1_programming_challenges/1_sum_of_two_digits.py
+++ week1_programming_challenges/1_sum_of_two_digits.py
@@ -1,12 +1,10 @@
 def ab_sum(numbers_list):
-
     sum_number = sum(numbers_list)
 
     return sum_number
 
 
 if __name__ == '__main__':
-
     input_string = input().split()
     numbers = [int(el) for el in input_string]
 
